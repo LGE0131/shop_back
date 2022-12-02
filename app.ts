9d8e93e903259c3b888f8f227288217a683744eb
LGE <igoeun0131@gmail.com>
2022-12-03 04:45:03 +0900

    일단 변경

--- app.ts
+++ app.ts
@@ -1,10 +1,30 @@
 import express, { Request, Response } from 'express'
+const axios = require('axios')
+const cors = require('cors')
+
+const router = express.Router()
 const app = express()
 const port = 3030
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+
+axios.defaults.withCredentials = true;
+
+// app.get('/', function(req: Request, res: Response){
+//   const userName = req.body._userName
+//   const userEmail = req.body._userEmail
+//   const userPw = req.body._userPw
+// console.log(userName);
+// console.log(userEmail);
+// console.log(userPw);
+// })
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
